cb02362cc35f0df2d418a6199a06c92ef23686f3
Taz <mataiscaldwell@gmail.com>
2022-12-02 14:17:29 -0600

    Added test file for python solution to palindrome problem.

--- problems/palindromeNumber/solution.py
+++ problems/palindromeNumber/solution.py
@@ -1,4 +1,19 @@
+# Class to solve palindrome problem with strings for leetcode.
+#
+# Resources: https://www.geeksforgeeks.org/reverse-string-python-5-different-ways/
+# Author: MaTais Caldwell (mkcaldwe@go.olemiss.edu)
+# Date: Dec 2, 2022
 class solution:
     def isPalindrome(self, x: int) -> bool:
-        solution = 0
+        userInput = input('\nGive me a number:')
+        userInput = str(userInput)
+
+        original = userInput
+        reverse = userInput[::-1]
+
+        if original == reverse:
+            solution = True
+        else:
+            solution = False
+
         return solution
